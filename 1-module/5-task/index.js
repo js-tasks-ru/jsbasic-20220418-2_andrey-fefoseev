9c5fa5591ab65ef03fb1069cf0c149722b6f15aa
Федосеев Андрей <anfdsv@yandex.ru>
2022-04-19 13:51:47 +0300

    занятие-1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    str = str.slice(0, maxlength - 1) + '…';
+    return str;
+  } else {
+    return str;
+  };
 }
+
+console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20))
