149787c1ac5bdccec4653d69c542e3d1f86854e2
Федосеев Андрей <anfdsv@yandex.ru>
2022-05-11 20:01:20 +0300

    4-занятие

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.getElementsByTagName('tbody')[0];
+  let trs = tbody.querySelectorAll('tr');
+
+  trs.forEach(row => {
+    const status = row.getElementsByTagName('td')[3];
+      if(!status.dataset.available) {
+          row.hidden = true;
+      }
+      else if(status.dataset.available === "true") {
+          row.classList.add('available');
+      }
+      else if(status.dataset.available === "false") {
+        row.classList.add('unavailable');
+      }
+      
+      const gender = row.getElementsByTagName('td')[2];
+      if(gender.textContent === 'm') {
+          row.classList.add('male');
+      }
+      else if(gender.textContent === 'f') {
+          row.classList.add('female');
+      }
+      
+      const age = row.getElementsByTagName('td')[1];
+      if(Number(age.textContent) < 18) {
+          row.style.textDecoration = 'line-through';
+      }
+  })
 }
